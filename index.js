3d45a6553ca8869a036ec69f24d52d98b6fafd80
Mithlesh kumar <61827773+Mithi-code@users.noreply.github.com>
2021-11-09 20:47:09 +0530

    Update index.js
    Fixed eslint error

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const modal = document.querySelector(".modal");
-const loginBtn = document.querySelector(".hamburger");
-const closeBtn = document.querySelector(".close");
-const nav = document.querySelector(".animation")
-const border = document.querySelector(".border1")
+const modal = document.querySelector('.modal');
+const loginBtn = document.querySelector(.hamburger');
+const closeBtn = document.querySelector('.close');
+const nav = document.querySelector('.animation');
+const border = document.querySelector('.border1');
 
-loginBtn.addEventListener("click", openModal);
-closeBtn.addEventListener("click", closeModal);
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
 
 function openModal() {
-  loginBtn.style.display = "none"
-  nav.style.display = "none"
-  modal.style.display = "block";
-  border.style.display = "block"
+  loginBtn.style.display = 'none';
+  nav.style.display = 'none';
+  modal.style.display = 'block';
+  border.style.display = 'block';
 }
 
 function closeModal() {
-  modal.style.display = "none";
-  loginBtn.style.display = "block"
-  nav.style.display = "block"
-  border.style.display = "none"
+  modal.style.display = 'none';
+  loginBtn.style.display = 'block';
+  nav.style.display = 'block';
+  border.style.display = 'none';
 }
