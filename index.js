0c9c3f925d1d5f64945c054293f6212667901ce7
Mithlesh kumar <61827773+Mithi-code@users.noreply.github.com>
2021-11-09 22:08:46 +0530

    Update index.js
    fixed linter error

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const closeBtn = document.querySelector('.close');
 const nav = document.querySelector('.animation');
 const border = document.querySelector('.border1');
 
-loginBtn.addEventListener('click', ()=>{
+loginBtn.addEventListener('click', () => {
   loginBtn.style.display = 'none';
   nav.style.display = 'none';
   modal.style.display = 'block';
   border.style.display = 'block';
 });
-closeBtn.addEventListener('click', ()=>{
+closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   loginBtn.style.display = 'block';
   nav.style.display = 'block';
   border.style.display = 'none';
 });
-
