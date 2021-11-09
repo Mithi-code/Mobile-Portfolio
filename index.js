a62b87334b75a82d1f5f2fc0e511b3409cbd145f
Mithlesh kumar <61827773+Mithi-code@users.noreply.github.com>
2021-11-09 20:59:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const modal = document.querySelector(".modal");
-const loginBtn = document.querySelector(".hamburger");
-const closeBtn = document.querySelector(".close");
-const nav = document.querySelector(".animation");
-const border = document.querySelector(".border1");
+const modal = document.querySelector('.modal');
+const loginBtn = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close');
+const nav = document.querySelector('.animation');
+const border = document.querySelector('.border1');
 
-loginBtn.addEventListener("click", function openModal() {
-  loginBtn.style.display = "none";
-  nav.style.display = "none";
-  modal.style.display = "block";
-  border.style.display = "block";
-});
-closeBtn.addEventListener("click", function closeModal() {
-  modal.style.display = "none";
-  loginBtn.style.display = "block";
-  nav.style.display = "block";
-  border.style.display = "none";
-});
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+function openModal() {
+    loginBtn.style.display = 'none';
+    nav.style.display = 'none';
+    modal.style.display = 'block';
+    border.style.display = 'block';
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+    loginBtn.style.display = 'block';
+    nav.style.display = 'block';
+    border.style.display = 'none';
+  }
