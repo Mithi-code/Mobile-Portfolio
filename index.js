6431d7a3b92f299050e6eaa4be9323d3e668f9bb
Mithlesh Kumar <mithlesh.9m@gmail.com>
2021-11-09 12:27:51 +0530

    added click event for menu

---
+++ index.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector(".modal");
+const loginBtn = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close");
+const nav = document.querySelector(".animation")
+const border = document.querySelector(".modal-border")
+
+loginBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  loginBtn.style.display = "none"
+  nav.style.display = "none"
+  modal.style.display = "block";
+  border.style.display = "block"
+}
+
+function closeModal() {
+  modal.style.display = "none";
+  loginBtn.style.display = "block"
+  nav.style.display = "block"
+}
